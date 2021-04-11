c78c7aca9ce7fc5b5af62e23ad64a77d9da82224
Robert <robert280402@gmail.com>
2021-04-11 23:30:31 +0500

    lab

--- webpage/index.php
+++ webpage/index.php
@@ -1,6 +1,54 @@
 <?php
 include('connection.php');
+$number =0 ;
+if($_SERVER["REQUEST_METHOD"]=="POST") {
+    $username = '';
+    $pwd = '';
+    $userDB='';
+    $showMessage=false;
+    ++$number;
+
+    @$isAvailable = true;
+    @$username = $_REQUEST["username"];
+    @$pwd = $_REQUEST["pwd"];
+
+
+
+    $db = new PDO("mysql:dbname=blog", "root", "555555");
+// Bool check for occurence in DB
+    $rows = $db->query("SELECT * FROM users WHERE username='$username'");
+
+    foreach ($rows as $row) {
+        $userDB = $row["username"];
+        $passDB = $row["password"];
+        $_SESSION["user"]=$row;
+    }
+
+    if (@$username == @$userDB) {
+
+
+        $isAvailable = true;
+        // check password
+        if ($pwd == @$passDB) {
+            $signedIn = true;
+
+
+        } else {
+            $signedIn = false;
+
+        }
+
+    } else if (@$username != @$userDB) {
+
+        @$isAvailable = false;
+        $showMessage= true;
+
+    }
+
+
+}
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
@@ -9,8 +57,14 @@ include('connection.php');
 	</head>
 	
 	<body>
-		<?php include('header.php'); ?>
+        <?php if(@$isAvailable == false && $number!=0  )  {
+            ?> <h2>  User with such username doesn't exit...
+        Please fill again</h2>
+        <?php }?>
+
 		<!-- Show this part if user is not signed in yet -->
+        <?php
+        if(@$signedIn==false ) { include('header.php'); ?>
 		<div class="twocols">
 			<form action="index.php" method="post" class="twocols_col">
 				<ul class="form">
@@ -20,14 +74,17 @@ include('connection.php');
 					</li>
 					<li>
 						<label for="pwd">Password</label>
-						<input type="password" name="pwd" id="pwd" />
+						<input type="password" name="pwd" id="pwd"/>
 					</li>
 					<li>
 						<label for="remember">Remember Me</label>
 						<input type="checkbox" name="remember" id="remember" checked />
+
 					</li>
 					<li>
 						<input type="submit" value="Submit" /> &nbsp; Not registered? <a href="register.php">Register</a>
+
+
 					</li>
 				</ul>
 			</form>
@@ -36,7 +93,9 @@ include('connection.php');
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur libero nostrum consequatur dolor. Nesciunt eos dolorem enim accusantium libero impedit ipsa perspiciatis vel dolore reiciendis ratione quam, non sequi sit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nobis vero ullam quae. Repellendus dolores quis tenetur enim distinctio, optio vero, cupiditate commodi eligendi similique laboriosam maxime corporis quasi labore!</p>
 			</div>
 		</div>
-		
+
+
+        <?php  }  else if ($signedIn == true )  { include('header.php'); ?>
 		<!-- Show this part after user signed in successfully -->
 		<div class="logout_panel"><a href="register.php">My Profile</a>&nbsp;|&nbsp;<a href="index.php?logout=1">Log Out</a></div>
 		<h2>New Post</h2>
@@ -69,5 +128,7 @@ include('connection.php');
 				<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
 			</div>
 		</div>
+        <?php }  false;
+        ?>
 	</body>
 </html>
--- webpage/register.php
+++ webpage/register.php
@@ -1,7 +1,29 @@
 <?php  
-
+@$password= '';
+@$confPass='';
 include('connection.php');
+$db = new PDO("mysql:dbname=blog","root","555555" );
+$insert_stmt= $db->prepare("INSERT INTO users(username,email,password,fullname,dob) VALUES(?,?,?,?,?)");
 
+if($_SERVER["REQUEST_METHOD"]=="POST");
+{
+
+
+    @$username = $_REQUEST["username"];
+    @$fullname = $_REQUEST["fullname"];
+    @$email = $_REQUEST["email"];
+    @$password = $_REQUEST["pwd"];
+    @$dob = $_REQUEST["dob"];
+    @$confPass = $_REQUEST["confirm_pwd"];
+
+
+
+    if($confPass == $password) {   @$passValid = true; }
+
+    $toProceed = !empty($username) && !empty($fullname) && !empty($email) && !empty($password) && @$passValid && !empty($dob);
+
+
+}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
@@ -32,14 +54,38 @@ include('connection.php');
 					</li>
 					<li>
 						<label for="pwd">Password</label>
-						<input type="password" name="pwd" id="pwd" required/>
+						<input type="password" name="pwd" ,id="pwd" required/>
 					</li>
+
+
+                    <li>
+                        <label for="dob">Date of birth(YYYY/MM/DD)</label>
+                        <input type="text" name="dob" id="dob" required/>
+                    </li>
 					<li>
 						<label for="confirm_pwd">Confirm Password</label>
 						<input type="password" name="confirm_pwd" id="confirm_pwd" required />
 					</li>
+
+                    <?php
+                        if($toProceed ==1)
+                        {
+
+                            // send data to data base
+                            $insert_stmt->bindParam(1,$username);
+                            $insert_stmt->bindParam(2,$email);
+                            $insert_stmt->bindParam(3,$password);
+                            $insert_stmt->bindParam(4,$fullname);
+                            $insert_stmt->bindParam(5,$dob);
+                            $insert_stmt->execute();
+                        }
+
+
+                    ?>
+
+
 					<li>
-						<input type="submit" value="Submit" /> &nbsp; Already registered? <a href="index.php">Login</a>
+						<input type="submit" value="Submit" /> <?php  if($toProceed==1) { header('Location:index.php');}  ?> Already registered? <a href="index.php">Login</a>
 					</li>
 				</ul>
 		</form>
